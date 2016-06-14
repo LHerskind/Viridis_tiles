8a38fcfe309985c9489e460fcf016be4225551f7
Thomas <s153047@student.dtu.dk>
2016-06-14 16:33:28 +0200

    lavet: tiles.xml, green/redview.xml, dele af GameActivity.class og tileView.class

---
+++ app/src/main/java/dtu/tilecolor/tileView.java
@@ -0,0 +1,21 @@
+package dtu.tilecolor;
+
+import android.graphics.Color;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.view.View;
+
+/**
+ * Created by Meowasaurus on 14-06-2016.
+ */
+public class tileView extends View {
+
+
+
+
+
+
+
+
+
+}
