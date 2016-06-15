0b9dd09ac3a6d4e318551aa7729e0b7a7dbec9c4
Lasse Herskind <lasse.herskind@gmail.com>
2016-06-15 16:07:16 +0200

    TileView updated

--- app/src/main/java/dtu/tilecolor/TileView.java
+++ app/src/main/java/dtu/tilecolor/TileView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -17,29 +18,38 @@ public class TileView extends View {
     private char c;
     private int i;
     private int j;
+    private int size;
 
     public TileView(Context context, char c, int i, int j){
         super(context);
+        this.size = 100;
         this.c=c;
         this.i=i;
         this.j=j;
     }
 
+    public TileView(Context context, char c, int i, int j, int size){
+        this(context,c,i,j);
+        this.size = size;
+    }
+
     @Override
     protected synchronized void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        int black = ContextCompat.getColor(getContext(), R.color.dark_background);
+        int red = ContextCompat.getColor(getContext(), R.color.red);
+        int green = ContextCompat.getColor(getContext(), R.color.green);
 
         if(c == 'w') {
-            mPaint.setColor(Color.BLACK);
+            mPaint.setColor(black);
         } else if (c == 'r'){
-            mPaint.setColor(Color.RED);
+            mPaint.setColor(red);
         } else if (c == 'g'){
-            mPaint.setColor(Color.GREEN);
+            mPaint.setColor(green);
             //} else if (c == 'p'){
             // TODO - player
         }
         int padding = 10;
-        int size = 100;
         int x = size*(j);
         int y = size*(i);
 
