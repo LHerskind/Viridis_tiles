d5c77d183a4c68ee4b8a361447fe72d135f43685
MatiasUnited <matiasdaugaard@gmail.com>
2016-06-15 14:16:41 +0200

    Merge branch 'master' of /Users/matiasdaugaard/AndroidStudioProjects/Viridis_tiles with conflicts.

--- app/src/main/java/dtu/tilecolor/tileView.java
+++ app/src/main/java/dtu/tilecolor/tileView.java
@@ -4,48 +4,38 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
-import android.widget.LinearLayout;
 
 /**
  * Created by Meowasaurus on 14-06-2016.
  */
-public class tileView extends View {
+public class TileView extends View {
 
     private final Paint mPaint = new Paint();
-    private LinearLayout mFrame;
     private char c;
     private int i;
     private int j;
 
-    public tileView(Context context, char c, int i, int j){
+    public TileView(Context context, char c, int i, int j){
         super(context);
-
-        this.c=c;
-        this.i=i;
-        this.j=j;
-
-
+        this.c = c;
+        this.i = i;
+        this.j = j;
 
     }
+
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
-        if(c == 'w') {
-            mPaint.setColor(Color.BLACK);
-        } else if (c == 'r'){
-            mPaint.setColor(Color.RED);
-        } else if (c == 'g'){
-            mPaint.setColor(Color.GREEN);
-            //} else if (c == 'p'){
-            // TODO - player
-        }
-
         mPaint.setStrokeWidth(0);
-
-        canvas.drawRect(16+(j-1)*(50+10),16+(i-1)*(50+10),50,50,mPaint);
-
+        if(c == 'r'){
+            mPaint.setColor(Color.RED);
+        }
+        if(c == 'g'){
+            mPaint.setColor(Color.GREEN);
+        }
+        canvas.drawRect(i,j,10,10,mPaint);
     }
 }
