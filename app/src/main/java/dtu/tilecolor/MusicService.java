083a680a760e8e0d44c1504eb9c8d5fc681e4cbb
SteenHLund <steen.h.lund@gmail.com>
2016-06-17 10:55:25 +0200

    Create MusicService.java

---
+++ app/src/main/java/dtu/tilecolor/MusicService.java
@@ -0,0 +1,55 @@
+package course.labs.activitylab;
+
+import android.app.Service;
+import android.media.MediaPlayer;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by steenlund on 6/15/16.
+ */
+public class MusicService extends Service {
+
+    public MediaPlayer player;
+
+    public void onCreate() {
+        super.onCreate();
+
+    }
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        float volume = intent.getIntExtra("volume", 0);
+        volume /= 100;
+        player = MediaPlayer.create(getBaseContext(), intent.getIntExtra("id", 0));
+        player.setLooping(true);
+        player.setVolume(volume, volume);
+        player.start();
+        return 1;
+    }
+
+    public void onStop() {
+
+    }
+    public void onPause() {
+
+    }
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+
+    public IBinder onBind(Intent arg0) {
+
+        return null;
+    }
+
+    public IBinder onUnBind(Intent arg0) {
+        return null;
+    }
+}
