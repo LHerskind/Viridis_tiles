99083d5d72bcec14c35df66f82b3b1dc93f57dd5
Zaggralez <jacob@pjetursson.com>
2016-06-20 12:33:55 +0200

    hh

---
+++ app/src/main/java/dtu/tilecolor/AI.java
@@ -0,0 +1,199 @@
+package dtu.tilecolor;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class AI {
+    private ArrayList<Integer> solution = new ArrayList<Integer>();
+    private int mapWidth = 5;
+
+    public AI(int[] initial) {
+        isSolvable(initial);
+
+    }
+
+    final PriorityQueue<State> queue = new PriorityQueue<State>(100, new Comparator<State>() {
+        @Override
+        // De forskellige states bliver sammenlignet i forhold til deres
+        // prioritet, dvs. deres værdi af f.
+        public int compare(State a, State b) {
+            return a.priority() - b.priority();
+        }
+    });
+    // // Her defineres et puzzle-State
+    class State {
+        int[] tiles;
+        int player;
+        int g; // Antallet af steps fra initial State.
+        int h; // Den estimerede, heuristiske værdi til målet (Manhattan
+        // Distance)
+        State previous; // Forrige state i vejen til løsningen
+
+        // A* prioritetsfunktionen (f=g+h).
+        int priority() {
+            return g + h;
+        }
+
+        // Her sættes start state
+        State(int[] initial) {
+            this.tiles = initial;
+            player = index(tiles, -1);
+            g = 0;
+            h = heuristic(tiles, player);
+            previous = null;
+        }
+
+        // Her defineres de senere states, hvor der byttes om på to givne tiles
+        // HOW DO THIS
+        State(State previous, int slideFromIndex) {
+            // Set the tiles to the previous ones
+            tiles = Arrays.copyOf(previous.tiles, previous.tiles.length);
+            player = slideFromIndex;
+            tiles[player]--;
+
+            g = previous.g + 1;
+            h = heuristic(tiles, player);
+            this.previous = previous;
+        }
+
+        boolean isGoal() {
+            if(h != 0) return false;
+            else return true;
+
+        }
+
+        State moveDown() {
+            if (player + mapWidth >= tiles.length || tiles[player + mapWidth] == 0) {
+                return null;
+            } else {
+                return new State(this, player + mapWidth);
+            }
+        }
+
+        State moveUp() {
+            if (player - mapWidth < 0  ||  tiles[player-mapWidth] == 0) {
+                return null;
+            } else {
+                return new State(this, player - mapWidth);
+            }
+        }
+
+        State moveRight() {
+            if (player + 1 >= tiles.length || player % 5 == 4 || tiles[player+1] == 0) {
+                return null;
+            } else {
+                return new State(this, player + 1);
+            }
+        }
+
+        State moveLeft() {
+            if (player <= 0 || player % 5 == 0 || tiles[player-1] == 0) {
+                return null;
+            } else {
+                return new State(this, player - 1);
+            }
+        }
+
+        public void toArray() {
+            if (previous != null) {
+                solution.add(player);
+                //System.out.println("g =" + g + "  |  h = " + h);
+                previous.toArray();
+            }
+        }
+
+    }
+
+    void addNextState(State nextState) {
+        if (nextState != null)
+            queue.add(nextState);
+    }
+
+    // Run the solver.
+    public void solve(int[] initial) {
+
+        // Her resettes de to lister
+        queue.clear();
+
+        // Start staten tilføjes til queuen.
+        queue.add(new State(initial));
+
+        while (!queue.isEmpty()) {
+
+            // Hent den første state i queuen
+            State state = queue.poll();
+
+            // Hvis dette er målet, er vi færdige, og state.toArray holder styr
+            // på routen derhen
+            if (state.isGoal()) {
+                state.toArray();
+                return;
+            }
+
+
+
+            // Her tilføjer vi om muligt 4 nye states til vores queue
+            addNextState(state.moveRight());
+            addNextState(state.moveDown());
+            addNextState(state.moveLeft());
+            addNextState(state.moveUp());
+        }
+    }
+
+    // Bruges til at finde spilleren
+    static int index(int[] tiles, int val) {
+        for (int i = 0; i < tiles.length; i++) {
+
+            if (tiles[i] == val) {
+                tiles[i] = 1;
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    static int heuristic(int[] tiles, int player) {
+        int h = 0;
+        for (int i = 0; i < tiles.length; i++) {
+
+            if (tiles[i] == 2) {
+                h++;
+                /*
+                if (Math.abs((player % 5) - (i % 5)) + Math.abs((player/5) - (i / 5)) <= 2) {
+                    h += Math.abs((player % 5) - (i % 5)) + Math.abs((player/5) - (i / 5));
+                }
+                */
+                 h += Math.abs((player % 5) - (i % 5)) + Math.abs((player/5) - (i / 5));
+            }
+        }
+        return h;
+    }
+
+    static int heuristic(int[] tiles) {
+        int h = 0;
+        for (int i = 0; i < tiles.length; i++) {
+
+            if (tiles[i] == 2) {
+                h++;
+            }
+        }
+        return h;
+    }
+
+    public ArrayList<Integer> getSolution() {
+        Collections.reverse(solution);
+
+        return solution;
+    }
+    public boolean isSolvable(int[] map) {
+        solve(map);
+        if(getSolution() == null) {
+            return false;
+        }
+        else return true;
+    }
+}
